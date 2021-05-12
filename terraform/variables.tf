b58db10ed77bd73f98458de0f0cb889b409fa7f2
Emilio <emilio@nclouds.com>
2021-05-12 01:38:16 -0600

    pushing terraform and custom image to stage

---
+++ terraform/variables.tf
@@ -0,0 +1,32 @@
+variable "project_id" {
+  default = "k8s-helm-challenge-edl"
+  description = "The project ID to host the cluster in"
+}
+variable "cluster_name" {
+  description = "The name for the GKE cluster"
+  default     = "edl-challenge"
+}
+variable "env_name" {
+  description = "The environment for the GKE cluster"
+  default     = "dev"
+}
+variable "region" {
+  description = "The region to host the cluster in"
+  default     = "us-east1"
+}
+variable "network" {
+  description = "The VPC network created to host the cluster in"
+  default     = "gke-network-edl"
+}
+variable "subnetwork" {
+  description = "The subnetwork created to host the cluster in"
+  default     = "gke-subnet-edl"
+}
+variable "ip_range_pods_name" {
+  description = "The secondary ip range to use for pods"
+  default     = "ip-range-pods-edl"
+}
+variable "ip_range_services_name" {
+  description = "The secondary ip range to use for services"
+  default     = "ip-range-services-edl"
+}
